1b81d82c5f52a798bdfab2eae7175f5a893fa860
ek0nn <msadhak123@hotmail.com>
2023-09-20 15:58:11 +0100

    eslint stuff

--- src/app/hero3.tsx
+++ src/app/hero3.tsx
@@ -11,10 +11,8 @@ const Hero3 = () => {
           Showcase
         </h1>
         <div className='flex flex-row mb-20'>
-        // eslint-disable-next-line react/jsx-no-comment-textnodes, react/jsx-no-comment-textnodes
         <p className=" text-1xl text-light mt-3 mb-8 w-[400px] ">
           Showoff your best gaming highlights,
-          // eslint-disable-next-line react/no-unescaped-entities
           "Whether it's a pentakill during an intense battle or a hilarious, unexpected in-game moment, we're here for it all! These moments define our gaming and entertainment experiences, sparking excitement, laughter, and unforgettable memories. They're the heart of why we love this world so much."
         </p>
         <Image className = "ml-20" src={'/images/val.jpg'} width={800} height={400} alt='game' />
